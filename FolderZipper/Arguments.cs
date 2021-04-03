45c819d8ae5183319f4367e9b0ea3e71d69a023e
Gilberto Magalhães Leite Filho <gilbertomagalhaeslf@gmail.com>
2021-04-03 14:57:14 +0100

    fixing typos

--- FolderZipper/Arguments.cs
+++ FolderZipper/Arguments.cs
@@ -10,12 +10,12 @@ namespace FolderZipper
         [Value(1, Required = true, HelpText = "Name of the file generated (without the extension).", MetaName = "name", MetaValue = "zipped_folder")]
         public string Name { get; set; }
         [Option('e', "extensions", Required = false, HelpText = "List of extensions to be excluded.")]
-        public IEnumerable<string> ExcludeExtensions { get; set; }
+        public IEnumerable<string> ExcludedExtensions { get; set; }
         [Option('d', "directories", Required = false, HelpText = "List of directories to be excluded.")]
-        public IEnumerable<string> ExcludeDirectories { get; set; }
+        public IEnumerable<string> ExcludedDirectories { get; set; }
         [Option('f', "files", Required = false, HelpText = "List of files to be excluded.")]
-        public IEnumerable<string> ExcludeFiles { get; set; }
-        [Option('o', "files", Required = false, HelpText = "List of files to be excluded.")]
+        public IEnumerable<string> ExcludedFiles { get; set; }
+        [Option('o', "files", Required = false, HelpText = "Path where the zip file should be moved")]
         public string Output { get; set; }
         [Option(Min = 1, Required = false, HelpText = "Additional parameters")]
         public IEnumerable<string> AdditionalParams { get; set; }
