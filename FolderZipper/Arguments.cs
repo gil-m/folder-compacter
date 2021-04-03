e8aa74534d405f10ee7a03635da72ea7296eddd2
Gilberto Magalhães Leite Filho <gilbertomagalhaeslf@gmail.com>
2021-04-03 14:08:11 +0100

    renaming files

---
+++ FolderZipper/Arguments.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace FolderZipper
+{
+    public class Arguments
+    {
+        [Value(0, Required = true, HelpText = "Path of the folder to be compacted.", MetaName = "path", MetaValue = @"C:\path\to\folder")]
+        public string Path { get; set; }
+        [Value(1, Required = true, HelpText = "Name of the file generated (without the extension).", MetaName = "name", MetaValue = "zipped_folder")]
+        public string Name { get; set; }
+        [Option('e', "extensions", Required = false, HelpText = "List of extensions to be excluded.")]
+        public IEnumerable<string> ExcludeExtensions { get; set; }
+        [Option('d', "directories", Required = false, HelpText = "List of directories to be excluded.")]
+        public IEnumerable<string> ExcludeDirectories { get; set; }
+        [Option('f', "files", Required = false, HelpText = "List of files to be excluded.")]
+        public IEnumerable<string> ExcludeFiles { get; set; }
+        [Option('o', "files", Required = false, HelpText = "List of files to be excluded.")]
+        public string Output { get; set; }
+        [Option(Min = 1, Required = false, HelpText = "Additional parameters")]
+        public IEnumerable<string> AdditionalParams { get; set; }
+    }
+}
---
+++ FolderZipper/Program.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using System;
+
+namespace FolderZipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Arguments>(args).WithParsed(o =>
+            {
+                Console.WriteLine("Starting execution with these parameters:");
+                Console.WriteLine($"{nameof(Arguments.Path)}: {o.Path}");
+                Console.WriteLine($"{nameof(Arguments.Name)}: {o.Name}");
+                Console.WriteLine($"{nameof(Arguments.ExcludeExtensions)}: {string.Join(",", o.ExcludeExtensions)}");
+                Console.WriteLine($"{nameof(Arguments.ExcludeDirectories)}: {string.Join(",", o.ExcludeDirectories)}");
+                Console.WriteLine($"{nameof(Arguments.ExcludeFiles)}: {string.Join(",", o.ExcludeFiles)}");
+                Console.WriteLine($"{nameof(Arguments.Output)}: {o.Output}");
+                Console.WriteLine($"{nameof(Arguments.AdditionalParams)}: {string.Join(",", o.AdditionalParams)}");
+            });
+        }
+    }
+}
