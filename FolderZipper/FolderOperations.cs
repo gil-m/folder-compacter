145d2918874a43285d5671ba62d9e9fc263f46b3
Gilberto Magalhães Leite Filho <gilbertomagalhaeslf@gmail.com>
2021-04-03 14:58:00 +0100

    including methods to copy all files to another temporary folder

---
+++ FolderZipper/FolderOperations.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FolderZipper
+{
+    public static class FolderOperations
+    {
+        /// <summary>
+        /// Copies the destination folder files and subdirectories to a temporary folder applying the exclusions.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="copySubDirs"></param>
+        public static DirectoryInfo DirectoryCopy(string source,
+                                          string destination,
+                                          string[] excludeDirectories,
+                                          string[] excludeFiles,
+                                          string[] excludeExtensions,
+                                          bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(source);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + source);
+            }
+
+            List<DirectoryInfo> dirs = dir.GetDirectories().ToList();
+
+            if (excludeDirectories != null && excludeDirectories.Any())
+                dirs = dirs.Where(x => !excludeDirectories.Contains(x.Name)).ToList();
+
+            // If the destination directory doesn't exist, create it.       
+            var dest = Directory.CreateDirectory(destination);
+
+            // Get the files in the directory and copy them to the new location.
+            List<FileInfo> files = dir.GetFiles().ToList();
+
+            if (excludeFiles != null && excludeFiles.Any())
+                files = files.Where(x => !excludeFiles.Contains(x.Name)).ToList();
+            if (excludeExtensions != null && excludeExtensions.Any())
+                files = files.Where(x => !excludeExtensions.Contains(x.Extension)).ToList();
+
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destination, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destination, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, excludeDirectories, excludeFiles, excludeExtensions, copySubDirs);
+                }
+            }
+
+            return dest;
+        }
+
+        public static void RecursiveDelete(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            RecursiveDelete(directory);
+        }
+
+        public static void RecursiveDelete(DirectoryInfo directory)
+        {
+            if (!directory.Exists)
+                return;
+
+            foreach (var dir in directory.EnumerateDirectories())
+            {
+                RecursiveDelete(dir.FullName);
+            }
+
+            directory.Delete(true);
+        }
+    }
+}
--- FolderZipper/Zipper.cs
+++ FolderZipper/Zipper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace FolderZipper
 {
     public class Zipper
     {
+        public readonly Arguments _arguments;
+        private readonly string _temporaryFolder;
+
+        public Zipper(Arguments arguments)
+        {
+            _arguments = arguments;
+            _temporaryFolder = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), ".temp_zip", Guid.NewGuid().ToString());
+        }
+
+        public void CompactFolder()
+        {
+            var excludedDirectories = _arguments.ExcludedDirectories.ToArray();
+            var excludedFiles = _arguments.ExcludedFiles.ToArray();
+            var excludedExtensions = _arguments.ExcludedExtensions.ToArray();
+
+            try
+            {
+                var dest = FolderOperations.DirectoryCopy(_arguments.Path, _temporaryFolder, excludedDirectories, excludedFiles, excludedExtensions, true);
+            }
+            finally
+            {
+                FolderOperations.RecursiveDelete(_temporaryFolder);
+            }
+
+        }
     }
 }
